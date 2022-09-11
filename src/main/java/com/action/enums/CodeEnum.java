3952653651547cecae583c8edd8c5843610ab9f2
wzg <com.briup>
2022-09-11 10:45:22 +0800

    update

--- src/main/java/com/action/enums/CodeEnum.java
+++
@@ -1,9 +0,0 @@
-package com.action.enums;
-
-/**
- * @author Dell
- * @create 2019-08-11 18:15
- */
-public interface CodeEnum {
-    Integer getCode();
-}
--- src/main/java/com/action/exception/ResponseBankException.java
+++
@@ -1,8 +0,0 @@
-package com.action.exception;
-
-/**
- * @author Dell
- * @create 2019-08-13 17:15
- */
-public class ResponseBankException extends RuntimeException {
-}
